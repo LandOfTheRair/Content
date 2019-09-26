83649e7e4a0c2a4f61486bc044a282e24c8e6546
Kyle J. Kemp <kyle@seiyria.com>
2019-09-26 16:04:12 -0500

    add spawner script, but need to fix maps first

---
+++ _transformers/spawner.js
@@ -0,0 +1,49 @@
+
+const YAML = require('yamljs');
+const recurse = require('recursive-readdir');
+const fs = require('fs');
+
+const conditionallyAddInformation = (spawner) => {
+};
+
+const validateSpawner = (spawner) => {
+  let hasBad = false;
+
+  if(!spawner.tag) {
+    console.error(`Spawner ${JSON.stringify(spawner)} has no tag!`);
+    hasBad = true;
+  }
+
+  if(hasBad) {
+    throw new Error(`Spawner ${JSON.stringify(spawner)} has failed validation!`);
+  }
+  
+  return true;
+};
+
+const merge = async () => {
+  try {
+    const files = await recurse(`spawners`);
+
+    const spawnerDatas = files.map(file => {
+      const spawnerData = YAML.load(file);
+
+      conditionallyAddInformation(spawnerData);
+      if(!validateSpawner(spawnerData)) throw new Error(`${spawnerData.tag} failed validation.`);
+
+      return spawnerData;
+    }).flat();
+
+    console.log(`Loading ${spawnerDatas.length} Spawners...`);
+
+    if(!fs.existsSync('_output')) fs.mkdirSync('_output');
+    fs.writeFileSync('_output/spawners.yml', YAML.stringify(spawnerDatas, 4));
+
+  } catch(e) {
+    console.error(e);
+    process.exit(-1);
+  }
+
+};
+
+merge();
