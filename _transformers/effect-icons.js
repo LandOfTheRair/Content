01f539558863ed1ec1a699ab65dc32b5bc231524
Kyle Kemp <kyle@seiyria.com>
2020-11-06 07:09:30 -0600

    Add basic effect icons

---
+++ _transformers/effect-icons.js
@@ -0,0 +1,21 @@
+
+const YAML = require('yamljs');
+const fs = require('fs');
+
+const merge = async () => {
+  try {
+
+    const files = fs.readdirSync('./effectIcons').map(f => YAML.load(`./effectIcons/${f}`));
+    const file = Object.assign({}, ...files);
+
+    if(!fs.existsSync('_output')) fs.mkdirSync('_output');
+    fs.writeFileSync('_output/effect-icons.json', JSON.stringify(file, null, 4));
+
+  } catch(e) {
+    console.error(e);
+    process.exit(-1);
+  }
+
+};
+
+merge();
