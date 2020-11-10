cf8568d6e8865aa88f7a7d8c4a201e46c9cdc0cf
Kyle Kemp <kyle@seiyria.com>
2020-11-10 17:51:24 -0600

    fix process, add some effects so the game can start

---
+++ _transformers/effect-data.js
@@ -0,0 +1,21 @@
+
+const YAML = require('yamljs');
+const fs = require('fs');
+
+const merge = async () => {
+  try {
+
+    const files = fs.readdirSync('./effectData').map(f => YAML.load(`./effectData/${f}`));
+    const file = Object.assign({}, ...files);
+
+    if(!fs.existsSync('_output')) fs.mkdirSync('_output');
+    fs.writeFileSync('_output/effect-data.json', JSON.stringify(file, null, 4));
+
+  } catch(e) {
+    console.error(e);
+    process.exit(-1);
+  }
+
+};
+
+merge();
