79adbe707e76260a6c68f2608208f5d1ef5fd400
Kyle Kemp <kyle@seiyria.com>
2020-12-11 18:31:33 -0600

    fix meta

--- _tasks/generate-meta.js
+++ _tasks/generate-meta.js
@@ -2,11 +2,10 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const { gitDescribeSync } = require('git-describe');
-
 async function getData() {
   let baseData = { hash: 'invalid' };
   try {
+    const { gitDescribeSync } = require('git-describe');
     baseData = gitDescribeSync({ dirtyMark: false, dirtySemver: false });
   } catch {
     const data = await fetch('https://api.github.com/repos/LandOfTheRair/Content/commits/master');
