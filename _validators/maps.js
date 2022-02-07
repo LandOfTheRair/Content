8644526844a1db612d99e3361191f6238c3271b0
Kyle Kemp <kyle@seiyria.com>
2022-02-06 21:08:29 -0600

    add semi

--- _validators/maps.js
+++ _validators/maps.js
@@ -13,7 +13,7 @@ const validate = async () => {
     if(json.version > 1.1) {
       throw new Error(`Map ${map} version is > 1.1! This is invalid because json2 parsing is not functional for this game.`);
     }
-  })
+  });
 
 };
 
