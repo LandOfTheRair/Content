ed330521493c4480fd9b3e794151875f4501d9ac
Kyle Kemp <kyle@seiyria.com>
2022-03-02 23:04:25 -0600

    add base config for solokar/etc, modify maps to accommodate, add transformer

--- _transformers/core.js
+++ _transformers/core.js
@@ -13,7 +13,28 @@ const merge = async () => {
       Object.keys(fData).forEach(dataKey => {
         if(!dataKey.startsWith('__')) return;
         delete fData[dataKey];
-      })
+      });
+
+      // special transforms for this file
+      if(file === 'rngdungeonconfig.yml') {
+        Object.keys(fData.decor).forEach(decorKey => fData.decor[decorKey] = fData.decor[decorKey].flat());
+
+        Object.keys(fData.floors).forEach(floorKey => {
+          if(fData.floors[floorKey].allowFluids) fData.floors[floorKey].fluids = fData.floors[floorKey].fluids.flat();
+
+          // don't flatten these, they're picked from intentionally
+          // if(fData.floors[floorKey].allowTrees) fData.floors[floorKey].trees = fData.floors[floorKey].trees.flat();
+
+          fData.floors[floorKey].decor = fData.floors[floorKey].decor.flat(Infinity);
+        });
+
+        fData.configs.roomDecor.forEach(({ decors }) => {
+          decors.forEach(decorConfig => {
+            decorConfig.decor = decorConfig.decor.flat();
+          });
+        });
+      }
+
       if(!fs.existsSync('_output')) fs.mkdirSync('_output');
       fs.writeFileSync(`_output/${file.split('.')[0]}.json`, JSON.stringify(fData, null, 4));
     });
