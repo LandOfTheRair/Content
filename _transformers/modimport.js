9d627259196e3047cbac2eee9577658d79bc2d84
Kyle Kemp <kyle@seiyria.com>
2024-08-20 16:03:44 -0500

    migrate core content into modkit

--- _transformers/modimport.js
+++ _transformers/modimport.js
@@ -1,5 +1,7 @@
 const fs = require("fs-extra");
 
+const OUTPUT_DIR = "_output";
+
 function toHash(arr, indexProp) {
   return arr.reduce(
     (prev, cur) => ({
@@ -10,7 +12,42 @@ function toHash(arr, indexProp) {
   );
 }
 
-const OUTPUT_DIR = "_output";
+function handleCoreFiles(files) {
+  files.forEach((file) => {
+    const fData = file.json;
+
+    Object.keys(fData).forEach((dataKey) => {
+      if (!dataKey.startsWith("__")) return;
+      delete fData[dataKey];
+    });
+
+    // special transforms for this file
+    if (file.name === "rngdungeonconfig") {
+      Object.keys(fData.decor).forEach(
+        (decorKey) => (fData.decor[decorKey] = fData.decor[decorKey].flat())
+      );
+
+      Object.keys(fData.floors).forEach((floorKey) => {
+        if (fData.floors[floorKey].allowFluids)
+          fData.floors[floorKey].fluids = fData.floors[floorKey].fluids.flat();
+
+        // don't flatten these, they're picked from intentionally
+        // if(fData.floors[floorKey].allowTrees) fData.floors[floorKey].trees = fData.floors[floorKey].trees.flat();
+
+        fData.floors[floorKey].decor =
+          fData.floors[floorKey].decor.flat(Infinity);
+      });
+
+      fData.configs.roomDecor.forEach(({ decors }) => {
+        decors.forEach((decorConfig) => {
+          decorConfig.decor = decorConfig.decor.flat();
+        });
+      });
+    }
+
+    fs.writeJSONSync(`${OUTPUT_DIR}/${file.name}.json`, file.json);
+  });
+}
 
 fs.ensureDirSync(OUTPUT_DIR);
 fs.ensureDirSync(`${OUTPUT_DIR}/maps`);
@@ -37,6 +74,8 @@ fs.readdirSync("mods").forEach((mod) => {
 
   fs.writeJSONSync(`${OUTPUT_DIR}/droptable-maps.json`, mapDrops);
   fs.writeJSONSync(`${OUTPUT_DIR}/droptable-regions.json`, regionDrops);
+
+  handleCoreFiles(modFile.cores);
 
   modFile.maps.forEach((mapData) => {
     const { name, map } = mapData;
