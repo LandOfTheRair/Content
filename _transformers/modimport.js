3518ffca08b98af71b30f5e703acb428d18ea5a8
Kyle J. Kemp <kyle@seiyria.com>
2024-09-23 18:13:32 -0500

    add ability to have multiple mods in folder

--- _transformers/modimport.js
+++ _transformers/modimport.js
@@ -46,44 +46,56 @@ fs.ensureDirSync(OUTPUT_DIR);
 fs.ensureDirSync(`${OUTPUT_DIR}/maps`);
 fs.ensureDirSync(`${OUTPUT_DIR}/simplemods`);
 
+const joinedMod = {};
+
 fs.readdirSync("mods").forEach((mod) => {
   const modFile = fs.readJSONSync(`mods/${mod}`);
+  if (mod.includes("BaseGameContent")) {
+    joinedMod.meta = modFile.meta;
+  }
 
-  fs.writeJSONSync(`${OUTPUT_DIR}/items.json`, modFile.items);
-  fs.writeJSONSync(`${OUTPUT_DIR}/npcs.json`, modFile.npcs);
-  fs.writeJSONSync(`${OUTPUT_DIR}/npc-scripts.json`, modFile.dialogs);
-  fs.writeJSONSync(`${OUTPUT_DIR}/recipes.json`, modFile.recipes);
-  fs.writeJSONSync(`${OUTPUT_DIR}/spawners.json`, modFile.spawners);
-  fs.writeJSONSync(
-    `${OUTPUT_DIR}/achievements.json`,
-    toHash(modFile.achievements, "name")
-  );
-
-  fs.writeJSONSync(`${OUTPUT_DIR}/quests.json`, toHash(modFile.quests, "name"));
-
-  const globalDroptable = modFile.drops.find((drop) => drop.isGlobal);
-  const mapDrops = modFile.drops.filter((drop) => drop.mapName);
-  const regionDrops = modFile.drops
-    .filter((drop) => drop.regionName)
-    .map((drop) => ({
-      ...drop,
-      drops: [...globalDroptable.drops, ...drop.drops],
-    }));
-
-  fs.writeJSONSync(`${OUTPUT_DIR}/droptable-maps.json`, mapDrops);
-  fs.writeJSONSync(`${OUTPUT_DIR}/droptable-regions.json`, regionDrops);
-
-  handleCoreFiles(modFile.cores);
-  handleSTEMs(modFile.stems);
-  handleTraitTrees(modFile.traitTrees);
-
-  modFile.maps.forEach((mapData) => {
-    const { name, map } = mapData;
-    fs.writeJSONSync(`${OUTPUT_DIR}/maps/${name}.json`, map);
+  Object.keys(modFile).forEach((key) => {
+    if (key === "meta") return;
+    joinedMod[key] ??= [];
+    joinedMod[key].push(...modFile[key]);
   });
 
   const strippedMod = structuredClone(modFile);
   delete strippedMod.meta._backup;
   strippedMod.maps = [];
+
   fs.writeJSONSync(`${OUTPUT_DIR}/simplemods/${mod}.json`, strippedMod);
+});
+
+fs.writeJSONSync(`${OUTPUT_DIR}/items.json`, joinedMod.items);
+fs.writeJSONSync(`${OUTPUT_DIR}/npcs.json`, joinedMod.npcs);
+fs.writeJSONSync(`${OUTPUT_DIR}/npc-scripts.json`, joinedMod.dialogs);
+fs.writeJSONSync(`${OUTPUT_DIR}/recipes.json`, joinedMod.recipes);
+fs.writeJSONSync(`${OUTPUT_DIR}/spawners.json`, joinedMod.spawners);
+fs.writeJSONSync(
+  `${OUTPUT_DIR}/achievements.json`,
+  toHash(joinedMod.achievements, "name")
+);
+
+fs.writeJSONSync(`${OUTPUT_DIR}/quests.json`, toHash(joinedMod.quests, "name"));
+
+const globalDroptable = joinedMod.drops.find((drop) => drop.isGlobal);
+const mapDrops = joinedMod.drops.filter((drop) => drop.mapName);
+const regionDrops = joinedMod.drops
+  .filter((drop) => drop.regionName)
+  .map((drop) => ({
+    ...drop,
+    drops: [...globalDroptable.drops, ...drop.drops],
+  }));
+
+fs.writeJSONSync(`${OUTPUT_DIR}/droptable-maps.json`, mapDrops);
+fs.writeJSONSync(`${OUTPUT_DIR}/droptable-regions.json`, regionDrops);
+
+handleCoreFiles(joinedMod.cores);
+handleSTEMs(joinedMod.stems);
+handleTraitTrees(joinedMod.traitTrees);
+
+joinedMod.maps.forEach((mapData) => {
+  const { name, map } = mapData;
+  fs.writeJSONSync(`${OUTPUT_DIR}/maps/${name}.json`, map);
 });
